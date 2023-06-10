145afb5fc22eaf40e9fb601c72e1568113b66de2
K-Mixail <131530@mail.ru>
2023-06-10 18:11:14 +0300

    тест шапки

--- layout/header.php
+++ layout/header.php
@@ -1,9 +1,23 @@
-
+<!-- <link rel="stylesheet" href="../styles/style.css"> -->
+ 
     <header class="header">
         <nav class="header-nav">
                 <ul class="sun-system">
                     Cолнечная система
-                    <!-- <li></li> -->
+                    <div class=""> 
+                        <ul class="sun-system__content" role="button" tabindex="0" onclick="this.parentElement.classList.toggle('is-open')">
+                            <a href="../pages/sunSystem/sunSystem.php">О солнечной системе</a>
+                            <a href="../pages/sunSystem/sun.php">Солнце</a>
+                            <a href="../pages/sunSystem/planetMerkur.php">Меркурий</a>
+                            <a href="../pages/sunSystem/planetVenus.php">Венера</a>
+                            <a href="../pages/sunSystem/planetEarth.php">Земля</a>
+                            <a href="../pages/sunSystem/planetMars.php">Марс</a>
+                            <a href="../pages/sunSystem/planetJupiter.php">Юпитер</a>
+                            <a href="../pages/sunSystem/planetSaturn.php">Сатурн</a>
+                            <a href="../pages/sunSystem/planetUranus.php">Уран</a>
+                            <a href="../pages/sunSystem/planetVenus.php">Нептун</a>
+                        </ul>
+                    </div>
                 </ul>
                 <ul class="stars">
                     Созвездия
